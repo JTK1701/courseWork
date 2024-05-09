21ee513545bdc8034a1612f9f36531a3434397a6
JTK1701 <jonmalkon@mail.ru>
2024-05-10 01:48:39 +0300

    course work lvl_3

--- src/Main.java
+++ src/Main.java
@@ -2,60 +2,122 @@ import java.util.Scanner;
 import java.util.Objects;
 
 public class Main {
-    public static Employee[] employeesArray = new Employee[10];
 
     public static void main(String[] args) {
-        employeesArray[0] = new Employee(1, 545455, "Natalia", "BlackWidow", "Romanoff");
-        employeesArray[1] = new Employee(2, 365454, "Nicholas", "Joseph", "Fury");
-        employeesArray[2] = new Employee(3, 2542546, "Alexander", "Goodwin", "Pierce");
-        employeesArray[3] = new Employee(4, 25425434, "Anthony", "Edward", "Stark");
-        employeesArray[4] = new Employee(5, 552528, "Brock", "CrossBones", "Ramlow");
-        employeesArray[5] = new Employee(1, 254252, "Clinton", " Francis", "Barton");
-        employeesArray[6] = new Employee(2, 364535, "Stiven", "Grant", "Rogers");
-        employeesArray[7] = new Employee(3, 25425, "James", "Bucky", "Barnes");
-        employeesArray[8] = new Employee(4, 254254, "Robert", "Bruce", "Banner");
-        employeesArray[9] = new Employee(5, 258546, "Jack", "Bear", "Rollins");
-        for (int i = 0; i < employeesArray.length; i++) {
-            System.out.println(employeesArray[i]);
-        }
-        System.out.println();
-        System.out.println();
-        double ollSalary = 0;
+        Employee[] employeeData = new Employee[10];
+        EmployeeBook newBook = new EmployeeBook(10);
+        //Основные сотрудники
+        employeeData[0] = new Employee(1, 60, "Natalia", "_BlackWidow_", "Romanoff");
+        employeeData[1] = new Employee(2, 80, "Nicholas", "Joseph", "Fury");
+        employeeData[2] = new Employee(3, 90, "Alexander", "_Goodwin_", "Pierce");
+        employeeData[3] = new Employee(4, 100, "Anthony", "Edward", "Stark");
+        employeeData[4] = new Employee(5, 50, "Brock", "_CrossBones_", "Ramlow");
+        employeeData[5] = new Employee(1, 70, "Clinton", " Francis", "Barton");
+        employeeData[6] = new Employee(2, 20, "Steven", "Grant", "Rogers");
+        employeeData[7] = new Employee(3, 10, "James", "_Bucky_", "Barnes");
+        employeeData[8] = new Employee(4, 40, "Robert", "Bruce", "Banner");
+        employeeData[9] = new Employee(5, 30, "Jack", "_Bear_", "Rollins");
+        //Дополнительные сотрудники
+        Employee newEmployee1 = new Employee(2, 40, "Loki", "Lodur", "Loptr");
+        Employee newEmployee2 = new Employee(4, 20, "Piter", "_Spidey_", "Parker");
 
-        {
-            for (int i = 0; i < employeesArray.length; i++) {
-                ollSalary += employeesArray[i].getSalary();
-            }
+        for (int i = 0; i < employeeData.length; i++) {
+            newBook.addToEmployeeBook(employeeData[i]);
         }
-        System.out.println("Затраты на заработную плату в месяц составляют - " + ollSalary);
         System.out.println();
-        Employee minSalary = employeesArray[0];
-        for (int i = 0; i <= employeesArray.length - 1; i++) {
-            if (employeesArray[i].getSalary() < minSalary.getSalary()) {
-                minSalary = employeesArray[i];
-            }
-        }
+        System.out.println();
+        float index = 1.6f;
 
-        System.out.println("Сотрудник с минимальной зарплатой - " + minSalary.getFirstName() + " " +
-                minSalary.getSurname() + " " + minSalary.getLastName() + ". Его заработная плата составляет - " +
-                minSalary.getSalary());
+        System.out.println("Затраты на зп в месяц составляют - " + newBook.calculateAllSalary());
         System.out.println();
-        Employee maxSalary = employeesArray[0];
-        for (int i = 0; i <= employeesArray.length - 1; i++) {
-            if (employeesArray[i].getSalary() > maxSalary.getSalary()) {
-                maxSalary = employeesArray[i];
-            }
-        }
-        System.out.println("Сотрудник с максимальной зарплатой - " + maxSalary.getFirstName() + " " +
-                maxSalary.getSurname() + " " + maxSalary.getLastName() + ". Его заработная плата составляет - " +
-                maxSalary.getSalary());
+        System.out.println("Распечатать массив всех сотрудников");
+        newBook.getEmployeeBookPrint();
+        System.out.println();
+        System.out.println();
+        System.out.println("Минимальная зп среди всех сотрудников");
+        newBook.calculateAndPrintMinSalary();
+        System.out.println();
+        System.out.println();
+        System.out.println("Максимальная зп среди всех сотрудников");
+        newBook.calculateAndPrintMaxSalary();
+        System.out.println();
+        System.out.println();
+        System.out.println("Средняя зп составляет - " + newBook.calculateAverageSalary(newBook.calculateAllSalary()));
+        System.out.println();
+        System.out.println();
+        System.out.println("Проиндексировать зп всех сотрудников");
+        newBook.calculateSalaryIndexation(index);
+        System.out.println();
+        System.out.println();
+        System.out.println("Создать новый массив для отдела");
+        newBook.excludeDepartmentsAndNewArray(3);
+        System.out.println();
+        System.out.println();
+        System.out.println("Добавить в массив и распечатать сотрудников отдела");
+        newBook.addOnDeptEmployeeAddAndPrint();
+        System.out.println();
+        System.out.println();
+        System.out.println("Затраты на зп отдела в месяц составляют - " + newBook.calculateInDeptAllSalary());
+        System.out.println();
+        System.out.println();
+        System.out.println("Максимальная зп в отделе");
+        newBook.calculateAndPrintInDeptMaxSalary();
+        System.out.println();
+        System.out.println();
+        System.out.println("минимальная зп в отделе");
+        newBook.calculateAndPrintInDeptMinSalary();
+        System.out.println();
+        System.out.println();
+        System.out.println("средняя зп сотрудников из отдела");
+        System.out.println(newBook.calculateInDeptAverageSalary(newBook.calculateInDeptAllSalary()));
+        System.out.println();
+        System.out.println();
+        System.out.println("Проиндексировать зп отдела");
+        newBook.calculateInDeptSalaryIndexation(index);
+        System.out.println();
+        System.out.println();
+        System.out.println("Распечатать сотрудников отдела");
+        newBook.printInDeptEmployees();
+        System.out.println();
+        System.out.println();
+        System.out.println("Зп меньше заданного значения");
+        newBook.calculateAndPrintListSalaryMoreThan(80);
+        System.out.println();
+        System.out.println();
+        System.out.println("Зп больше заданного числа");
+        newBook.calculateAndPrintListSalaryLessThan(80);
+        System.out.println();
+        System.out.println();
+        System.out.println("Увеличить массив");
+        newBook.stretchArrayEmployees();
+        System.out.println();
+        System.out.println();
+        System.out.println("Распечатать всех сотрудников");
+        newBook.getEmployeeBookPrint();
+        System.out.println();
+        System.out.println();
+        System.out.println("Добавить нового сотрудника");
+        newBook.addNewEmployee(newEmployee1);
+        System.out.println();
+        System.out.println();
+        System.out.println("Добавить нового сотрудника");
+        newBook.addNewEmployee(newEmployee2);
+        System.out.println();
+        System.out.println();
+        System.out.println("Распечатать всех сотрудников");
+        newBook.getEmployeeBookPrint();
+        System.out.println();
+        System.out.println();
+        System.out.println("Найти сотрудника по id");
+        newBook.searchId(7);
+        System.out.println();
+        System.out.println();
+        System.out.println("Удалить сотрудника");
+        newBook.deleteThisEmployee(3);
+        System.out.println();
+        System.out.println();
+        System.out.println("Найти сотрудника по id");
+        newBook.searchId(4);
 
-        double middleSalary = ollSalary/employeesArray.length;
-        System.out.println("Средняя заработная плата составляет - " + middleSalary);
-        System.out.println();
-        for (int i = 0; i < employeesArray.length; i++) {
-            System.out.println("ФИО сотрудника - " + employeesArray[i].getFirstName() + " " +
-                    employeesArray[i].getSurname() + " " + employeesArray[i].getLastName());
-        }
     }
 }
